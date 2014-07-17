eeb6c1d9504c069b0178b333ce7cdca0c022d022
sandiegodj <davidjanczyn@gmail.com>
2014-07-16 22:52:54 -0700

    caesars cypher is complete

---
+++ caesar.rb
@@ -0,0 +1,18 @@
+def caesar_cypher(message, cypher)
+cypher = cypher.to_i
+encrypted_message = []
+
+message.each_byte do |letter|
+	if letter >= 65 and letter <= 90
+		encrypted_message.push(((((letter - 65) + cypher) % 26) + 65).chr)
+	elsif letter >= 97 and letter <= 122
+		encrypted_message.push(((((letter - 97) + cypher) % 26) + 97).chr)
+	else
+		encrypted_message.push(letter.chr)
+	end
+end
+ puts encrypted_message.join
+end
+
+caesar_cypher("Hello, Mother-efers!", 22)
+
